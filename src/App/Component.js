a24ce164d600466d3fefa032c6d1bf440eaae206
Chris Brett <chris.brett@citizensadvice.org.uk>
2019-07-03 20:11:04 +0100

    Hooks: The basics of using useState using a counter.

--- src/App/Component.js
+++ src/App/Component.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  // pattern #1
+  // const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() =>
+  //         setCount((currentState) => ({
+  //           ...currentState,
+  //           count: currentState.count + 1,
+  //         }))
+  //       }>
+  //       Increment
+  //     </button>
+
+  //     <div>Count1: {count}</div>
+  //     <div>Count2: {count2}</div>
+  //   </div>
+  // );
+
+  // pattern #2
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <button
+        onClick={() => {
+          setCount((c) => c + 1);
+          setCount2((c) => c + 1);
+        }}>
+        Increment
+      </button>
+
+      <div>Count1: {count}</div>
+      <div>Count2: {count2}</div>
     </div>
   );
-}
+};
 
 export default App;
