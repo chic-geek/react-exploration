f4d44268adcb0a933401924447caa2d2120d2f5d
Chris Brett <c.brett84@gmail.com>
2020-06-11 09:44:19 +0100

    Testing out the use of use-reactive-form hook.

--- src/App/Component.js
+++ src/App/Component.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import { array, object, string } from 'yup';
+import { useReactiveForm } from 'use-reactive-form';
 
-function App() {
+export default function App() {
+  const fields = {
+    user: '',
+    books: [
+      {
+        title: '',
+        author: '',
+      }
+    ]
+  }
+
+  const schema = object().shape({
+    user: string().max(20, 'Character limit exceeded')
+      .required('This field is required'),
+    books: array().of(object().shape({
+      title: string().required('This field is required'),
+      author: string().required('This field is required'),
+    })),
+  });
+
+  const config = {
+    fields: fields,
+    schema: schema
+  };
+
+  const { values, ref, validate, errors } = useReactiveForm(config);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      console.log(values);
+    } else {
+      console.log(errors);
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+    <form ref={ref} onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="user">User</label>
+        <input type="text" name="user" id="user" defaultValue={values.name} />
+        {errors.user.error && <p> {errors.user.error} </p>}
+      </div>
 
-export default App;
+      {values.books.map((b, i) => (
+        <div key={`book${i}`}>
+          <input type="text" name={`books_${i}_title`} />
+          <input type="text" name={`books_${i}_author`} />
+        </div>
+      ))}
+
+      <button type="submit" onClick={onSubmit}>Submit</button>
+    </form>
+  )
+}
